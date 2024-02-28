f070a55371494f65c99fea6c39e825d246bee57c
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-02-28 00:20:06 -0300

    feat: adicionando as variaveis curso e descrição

--- gestao-curso/src/main/java/com/senac/gestaocurso/models/Turma.java
+++ gestao-curso/src/main/java/com/senac/gestaocurso/models/Turma.java
@@ -5,20 +5,22 @@ import java.util.List;
 
 public class Turma {
 
-    private String nomeTurma;
+    private String nome;
     private List<Funcionario> alunos;
     private LocalDate dataInicio;
     private LocalDate dataFinal;
+    private Curso curso;
+    private String descricao;
 
     public Turma() {
     }
 
-    public String getNomeTurma() {
-        return nomeTurma;
+    public String getNome() {
+        return nome;
     }
 
-    public void setNomeTurma(String nomeTurma) {
-        this.nomeTurma = nomeTurma;
+    public void setNome(String nomeTurma) {
+        this.nome = nomeTurma;
     }
 
     public List<Funcionario> getAlunos() {
@@ -45,10 +47,26 @@ public class Turma {
         this.dataFinal = dataFinal;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
     @Override
     public String toString() {
         return "Turma{" +
-                "nomeTurma='" + nomeTurma + '\'' +
+                "nomeTurma='" + nome + '\'' +
                 ", alunos=" + alunos +
                 ", dataInicio=" + dataInicio +
                 ", dataFinal=" + dataFinal +
