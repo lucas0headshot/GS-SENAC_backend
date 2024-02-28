cd83c7e9ef8f55db7bf93b50308d05b4520af815
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-02-28 00:16:53 -0300

    feat: implementando classe de Frequencia

--- gestao-curso/src/main/java/com/senac/gestaocurso/models/Frequencia.java
+++ gestao-curso/src/main/java/com/senac/gestaocurso/models/Frequencia.java
@@ -2,4 +2,43 @@ package com.senac.gestaocurso.models;
 
 
 public class Frequencia {
+    private Boolean frequencia;
+    private Aula diaDaAula;
+    private Funcionario aluno;
+
+    public Frequencia() {
+    }
+
+    public Boolean getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(Boolean frequencia) {
+        this.frequencia = frequencia;
+    }
+
+    public Aula getDiaDaAula() {
+        return diaDaAula;
+    }
+
+    public void setDiaDaAula(Aula diaDaAula) {
+        this.diaDaAula = diaDaAula;
+    }
+
+    public Funcionario getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Funcionario aluno) {
+        this.aluno = aluno;
+    }
+
+    @Override
+    public String toString() {
+        return "Frequencia{" +
+                "frequencia=" + frequencia +
+                ", diaDaAula=" + diaDaAula +
+                ", aluno=" + aluno +
+                '}';
+    }
 }
