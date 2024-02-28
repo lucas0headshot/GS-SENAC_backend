203428eb0da6a35934e9d85d4bd5bb9282bd7653
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-02-28 00:14:41 -0300

    refactor: refatorando classe de aula implementando getters e setters

--- gestao-curso/src/main/java/com/senac/gestaocurso/models/Aula.java
+++ gestao-curso/src/main/java/com/senac/gestaocurso/models/Aula.java
@@ -1,7 +1,35 @@
 package com.senac.gestaocurso.models;
 
+import java.time.LocalDate;
+
 public class Aula {
     private Materia materia;
-    private Integer qtdAulas;
+    private LocalDate diaDaAula;
 
+    public Aula() {
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public LocalDate getDiaDaAula() {
+        return diaDaAula;
+    }
+
+    public void setDiaDaAula(LocalDate diaDaAula) {
+        this.diaDaAula = diaDaAula;
+    }
+
+    @Override
+    public String toString() {
+        return "Aula{" +
+                "materia=" + materia +
+                ", diaDaAula=" + diaDaAula +
+                '}';
+    }
 }
