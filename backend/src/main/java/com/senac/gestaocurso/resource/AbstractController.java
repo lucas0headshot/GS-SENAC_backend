440dfe3fc377ba79b14d03e55e116f726c0ba6c4
lucas0headshot <lucas0headshot@gmail.com>
2024-03-20 00:02:11 -0300

    feat: add AbstractController
    Adicionar AbstractController, cujo abstrai o tratamento de exceções

---
+++ backend/src/main/java/com/senac/gestaocurso/resource/AbstractController.java
@@ -0,0 +1,55 @@
+package com.senac.gestaocurso.resource;
+
+
+
+import com.senac.gestaocurso.enterprise.exception.BusinessException;
+import com.senac.gestaocurso.enterprise.exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+
+abstract public class AbstractController {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        List<String> collect = ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(p -> ((FieldError) p).getField() + " "
+                        + p.getDefaultMessage())
+                .collect(Collectors.toList());
+        errors.put("erro", collect.toString());
+
+        return errors;
+    }
+
+
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(ValidationException.class)
+    public Map<String, String> handleValidationExceptions(ValidationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("erro", ex.getMessage());
+
+        return errors;
+    }
+
+
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(BusinessException.class)
+    public Map<String, String> handleBusinessExceptions(ValidationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("erro", ex.getMessage());
+
+        return errors;
+    }
+}
