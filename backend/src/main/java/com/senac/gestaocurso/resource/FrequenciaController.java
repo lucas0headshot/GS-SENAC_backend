4d23b9132e0206e9d88f58f43e447daefd64e970
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-19 22:46:17 -0300

    fix: arrumando rota do RequestMapping

--- backend/src/main/java/com/senac/gestaocurso/resource/FrequenciaController.java
+++ backend/src/main/java/com/senac/gestaocurso/resource/FrequenciaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 @RestController
-@RequestMapping("api/frquencia")
+@RequestMapping("api/frequencia")
 public class FrequenciaController {
     @Autowired
     private FrequenciaService frequenciaService;
