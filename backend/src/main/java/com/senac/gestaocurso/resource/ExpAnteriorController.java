c93f2f92d88a59e72f35d2060a9e8014d0f345ef
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-19 22:45:43 -0300

    feat: implementando RequestMapping

--- backend/src/main/java/com/senac/gestaocurso/resource/ExpAnteriorController.java
+++ backend/src/main/java/com/senac/gestaocurso/resource/ExpAnteriorController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/api/experiencias")
 public class ExpAnteriorController {
     @Autowired
     private ExpAnteriorService expAnteriorService;
