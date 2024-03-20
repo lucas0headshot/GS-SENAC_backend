4a139e9a42f4ab36ea635bc38ec7835586b4d16c
lucas0headshot <lucas0headshot@gmail.com>
2024-03-20 01:13:56 -0300

    feat: add validação nota min & max na Avaliação
    Adicionar validação de nota mínima e máxima na classe Avaliação

--- backend/src/main/java/com/senac/gestaocurso/service/AvaliacaoService.java
+++ backend/src/main/java/com/senac/gestaocurso/service/AvaliacaoService.java
@@ -1,28 +1,40 @@
 package com.senac.gestaocurso.service;
 
 
+
 import com.senac.gestaocurso.models.Avaliacao;
 import com.senac.gestaocurso.repository.AvaliacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.Optional;
+
+
 
 @Service
 public class AvaliacaoService {
-
     @Autowired
     private AvaliacaoRepository avaliacaoRepository;
-    public Avaliacao salvar(Avaliacao entity) {return avaliacaoRepository.save(entity);}
+
+
+
+    public Avaliacao salvar(Avaliacao entity) {
+        return avaliacaoRepository.save(entity);
+    }
+
+
+
     public Page<Avaliacao> buscaTodos(Pageable pageable) {
         return avaliacaoRepository.findAll(pageable);
     }
+
     public Avaliacao buscaPorId(Long id) {
         return avaliacaoRepository.findById(id).orElse(null);
     }
+
+
+
     public Avaliacao alterar(Long id, Avaliacao alterado) {
         Optional<Avaliacao> encontrado = avaliacaoRepository.findById(id);
         if (encontrado.isPresent()) {
@@ -32,12 +44,14 @@ public class AvaliacaoService {
             avaliacao.setMateria(alterado.getMateria());
             avaliacao.setNome(alterado.getNome());
             avaliacao.setNota(avaliacao.getNota());
+
             return avaliacaoRepository.save(avaliacao);
         }
         return null;
     }
+
+
+
     public void remover(Long id) {avaliacaoRepository.deleteById(id);
     }
-
-
 }
