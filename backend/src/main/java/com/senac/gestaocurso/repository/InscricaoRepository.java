b56f9a2c8621f4d4123a9e9995fd936d0318ef17
lucas0headshot <lucas0headshot@gmail.com>
2024-03-20 01:15:34 -0300

    feat: add RN limite inscrição na turma
    Adicionar validação da RN de limite de inscrição em uma Turma

--- backend/src/main/java/com/senac/gestaocurso/repository/InscricaoRepository.java
+++ backend/src/main/java/com/senac/gestaocurso/repository/InscricaoRepository.java
@@ -1,9 +1,18 @@
 package com.senac.gestaocurso.repository;
 
+
+
 import com.senac.gestaocurso.models.Inscricao;
+import com.senac.gestaocurso.models.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
+
+
+@Repository
 public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
+    List<Inscricao> findAllByTurma(Turma turma);
 }
