7f186098fb26c69fcb001250e24b7fafa5ac2314
lucas0headshot <lucas0headshot@gmail.com>
2024-03-21 01:01:34 -0300

    feat: add métodos FuncionarioRepository
    Adicionar métodos no FuncionarioRepository

--- backend/src/main/java/com/senac/gestaocurso/repository/FuncionarioRepository.java
+++ backend/src/main/java/com/senac/gestaocurso/repository/FuncionarioRepository.java
@@ -9,4 +9,14 @@ import java.util.Optional;
 @Repository
 public interface FuncionarioRepository extends JpaRepository <Funcionario, Long> {
     Optional<Funcionario> findByCpf(String cpf);
+
+    Optional<Funcionario> findByRg(String rg);
+
+    Optional<Funcionario> findByTituloEleitor(String tituloEleitor);
+
+    Optional<Funcionario> findByPisPasep(String pisPasep);
+
+    Optional<Funcionario> findByCnh(String cnh);
+
+    Optional<Funcionario> findByMatricula(String matricula);
 }
