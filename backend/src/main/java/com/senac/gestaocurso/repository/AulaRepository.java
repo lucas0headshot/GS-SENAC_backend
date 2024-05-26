24104c8565823284adb67b4314581b2688dd8fe3
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-05-26 19:19:02 -0300

    feat: adicionando metodos de busca para validações

--- backend/src/main/java/com/senac/gestaocurso/repository/AulaRepository.java
+++ backend/src/main/java/com/senac/gestaocurso/repository/AulaRepository.java
@@ -5,6 +5,12 @@ import com.senac.gestaocurso.models.Aula;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface AulaRepository extends JpaRepository<Aula, Long>, CustomQuerydslPredicateExecutor<Aula> {
+
+    Aula findByMateria(Long idMateria);
+    Aula findByDia(LocalDate dia);
+    Aula findByProfessor(Long idProfessor);
 }
