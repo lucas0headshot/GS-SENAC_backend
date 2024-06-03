683ba53877a4b2cf57f9b0d68800b877e4613f5f
Lucas Ronchi <lucas0headshot@gmail.com>
2024-06-02 23:26:13 -0300

    refactor: upd enum StatusInscricao
    Adicionar ANALISE_FINANCEIRO, APROVADO_FINANCEIRO, REPROVADO_FINANCEIRO & ANALISE.

--- backend/src/main/java/com/senac/gestaocurso/enums/StatusInscricao.java
+++ backend/src/main/java/com/senac/gestaocurso/enums/StatusInscricao.java
@@ -1,6 +1,11 @@
 package com.senac.gestaocurso.enums;
 
 public enum StatusInscricao {
+    ANALISE_FINANCEIRO,
+    APROVADO_FINANCEIRO,
+    REPROVADO_FINANCEIRO,
+    ANALISE,
     APROVADA,
-    REPROVADA
+    REPROVADA,
+    FINALIZADA
 }
