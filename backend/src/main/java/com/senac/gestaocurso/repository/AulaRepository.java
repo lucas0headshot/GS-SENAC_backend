7fee128cfd1fc5687a4809b8dd3f07b36ead130b
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-06-12 20:51:05 -0300

    fix: removendo metodos que estavam NOCAUTEANDO a aplicação

--- backend/src/main/java/com/senac/gestaocurso/repository/AulaRepository.java
+++ backend/src/main/java/com/senac/gestaocurso/repository/AulaRepository.java
@@ -1,6 +1,7 @@
 package com.senac.gestaocurso.repository;
 
 import com.senac.gestaocurso.enterprise.CustomQuerydslPredicateExecutor;
+import com.senac.gestaocurso.models.Funcionario;
 import com.senac.gestaocurso.models.domain.Aula;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,8 +10,5 @@ import java.time.LocalDate;
 
 @Repository
 public interface AulaRepository extends JpaRepository<Aula, Long>, CustomQuerydslPredicateExecutor<Aula> {
-
-    Aula findByMateria(Long idMateria);
     Aula findByDia(LocalDate dia);
-    Aula findByProfessor(Long idProfessor);
 }
