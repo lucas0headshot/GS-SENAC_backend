77f6b21c62192cb679f6c9285f3d35b1d077702d
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-25 20:01:17 -0300

    feat: Configurando WebMvc para implementar o registro do rate limiter

---
+++ backend/src/main/java/com/senac/gestaocurso/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.senac.gestaocurso;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    public WebMvcConfig(RateLimitInterceptor rateLimitInterceptor) {
+        this.rateLimitInterceptor = rateLimitInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor);
+    }
+}
