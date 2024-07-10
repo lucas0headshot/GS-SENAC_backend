e9dfb424d3d1c685e26fe5423277af190c78fac3
Carrerogabriel <132373775+Carrerogabriel@users.noreply.github.com>
2024-07-10 10:28:21 -0300

    AulaDto
    Adição de atributos a classe AulaDto

--- backend/src/main/java/com/senac/gestaocurso/dto/AulaDto.java
+++ backend/src/main/java/com/senac/gestaocurso/dto/AulaDto.java
@@ -1,15 +1,22 @@
 package com.senac.gestaocurso.dto;
 
 import com.senac.gestaocurso.models.domain.Aula;
+import com.senac.gestaocurso.models.domain.Materia;
+
 import java.time.LocalDate;
 
 public class AulaDto {
+
+    private  String nomeProfessor;
+    private  String nome;
     private  Long id;
     private  LocalDate dia;
 
     public AulaDto() {}
 
-    public AulaDto(Long id, LocalDate dia) {
+    public AulaDto(String nomeProfessor ,String nome, Long id, LocalDate dia) {
+        this.nomeProfessor = nomeProfessor;
+        this.nome = nome;
         this.id = id;
         this.dia = dia;
     }
@@ -26,12 +33,32 @@ public class AulaDto {
         return dia;
     }
 
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public void setDia(LocalDate dia) {
         this.dia = dia;
     }
 
+
         public static AulaDto fromEntity(Aula entity){
             return new AulaDto(
+
+                    entity.getProfessor().getNome(),
+                    entity.getMateria().getNome(),
                     entity.getId(),
                     entity.getDia()
             );
